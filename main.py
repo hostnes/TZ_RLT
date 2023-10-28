7b510879eb71805893fa90e4d1aebaa2c8da803f
hostnes <lapanik2708@gmail.com>
2023-10-28 21:21:04 +0300

    ez

--- main.py
+++ main.py
@@ -1,78 +1,75 @@
-# import pymongo
-# import bson
-# import datetime
-#
-# client = pymongo.MongoClient("mongodb://localhost:27017")
-# db = client['admin']
-# coll = db['coll']
-#
-# with open('sample_collection.bson', 'rb') as f:
-#     bson_data = bson.decode_all(f.read())
-#
-# # print(bson.encode(bson_data))
-# # data = bson.decode(bson_data)
-#
-# coll.insert_many(bson_data)
+import bson
+import datetime
 import asyncio
+import json
 import sys
-
+import logging
 import pymongo
 
 from datetime import datetime
-
-import logging
-
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import state
 from dateutil.relativedelta import relativedelta
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
 from aiogram import Bot, Dispatcher, types
 
-from aiogram.dispatcher.filters.state import State, StatesGroup
 
-from state import SendMessage
-
-bot = Bot(token="6719983801:AAFWW8Rq_4GhqUFpZ8Sv-NkTRYT_izvJIBg")
-dp = Dispatcher(bot, storage=MemoryStorage())
-
+"""Запись данных с bson файла в mongodb"""
 client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client['admin']
 coll = db['coll']
 
+with open('sample_collection.bson', 'rb') as f:
+    bson_data = bson.decode_all(f.read())
+
+coll.insert_many(bson_data)
+"""######################################"""
+
+
+bot = Bot(token="6719983801:AAFWW8Rq_4GhqUFpZ8Sv-NkTRYT_izvJIBg")
+dp = Dispatcher(bot, storage=MemoryStorage())
+
 
 @dp.message_handler(commands=["start"])
-async def start(message: types.Message, state: FSMContext):
+async def start(message: types.Message):
     await message.answer(text='Дарова')
-    await state.set_state(SendMessage.get_message.state)
 
 
-@dp.message_handler(state=SendMessage.get_message)
-async def msg(message: types.Message, state: FSMContext):
-    date_format = "%Y-%m-%dT%H:%M:%S"
-    print(message.from_user.id)
-
+@dp.message_handler(content_types=['any'])
+async def msg(message: types.Message):
+    start_date_format = "%Y-%m-%dT%H:%M:%S"
+    date_format = "%Y-%m-%d %H:%M:%S"
+    insert_value = eval(message.text)
+    is_valid_msg = False
     try:
-        insert_value = eval(message.text)
-
-        dt_from = datetime.strptime(insert_value['dt_from'], date_format)
-        dt_upto = datetime.strptime(insert_value['dt_upto'], date_format)
+        new_dt_from = datetime.strptime(str(datetime.strptime(insert_value['dt_from'], start_date_format)), date_format)
+        new_dt_upto = datetime.strptime(str(datetime.strptime(insert_value['dt_upto'], start_date_format)), date_format)
         group_type = insert_value['group_type']
+        is_valid_msg = True
+    except:
+        pass
 
-        date_format = "%Y-%m-%d %H:%M:%S"
-
-        new_dt_from = datetime.strptime(str(dt_from), date_format)
-        new_dt_upto = datetime.strptime(str(dt_upto), date_format)
-
+    if is_valid_msg == True:
         full_dates = []
         for i in coll.find():
             if new_dt_from <= i['dt']:
                 if i['dt'] <= new_dt_upto:
-                    # print(i['dt'])
                     full_dates.append(i)
 
         dataset = []
         labels = []
+
+        if group_type == 'hour':
+            now = new_dt_from
+            while True:
+                res = 0
+                if now <= new_dt_upto:
+                    for i in full_dates:
+                        if i['dt'].year == now.year and i['dt'].month == now.month and i['dt'].day == now.day and i['dt'].hour == now.hour:
+                            res += i['value']
+                    dataset.append(res)
+                    labels.append(now.strftime(start_date_format))
+                    now += relativedelta(hours=1)
+                else:
+                    break
 
         if group_type == 'day':
             now = new_dt_from
@@ -80,12 +77,10 @@ async def msg(message: types.Message, state: FSMContext):
                 res = 0
                 if now <= new_dt_upto:
                     for i in full_dates:
-                        if i['dt'].year == now.year:
-                            if i['dt'].month == now.month:
-                                if i['dt'].day == now.day:
-                                 res += i['value']
+                        if i['dt'].year == now.year and i['dt'].month == now.month and i['dt'].day == now.day:
+                            res += i['value']
                     dataset.append(res)
-                    labels.append(str(now))
+                    labels.append(now.strftime(start_date_format))
                     now += relativedelta(days=1)
                 else:
                     break
@@ -96,38 +91,18 @@ async def msg(message: types.Message, state: FSMContext):
                 res = 0
                 if now <= new_dt_upto:
                     for i in full_dates:
-                        if i['dt'].year == now.year:
-                            if i['dt'].month == now.month:
+                        if i['dt'].year == now.year and i['dt'].month == now.month:
                                 res += i['value']
                     dataset.append(res)
-                    labels.append(str(now))
+                    labels.append(now.strftime(start_date_format))
                     now += relativedelta(months=1)
                 else:
                     break
 
-        if group_type == 'hour':
-            now = new_dt_from
-            while True:
-                res = 0
-                if now <= new_dt_upto:
-                    for i in full_dates:
-                        if i['dt'].year == now.year:
-                            if i['dt'].month == now.month:
-                                if i['dt'].day == now.day:
-                                    if i['dt'].hour == now.hour:
-                                        res += i['value']
-                    dataset.append(res)
-                    labels.append(str(now))
-                    now += relativedelta(hours=1)
-                else:
-                    break
-
-        diction = {'dataset': dataset, 'labels': labels}
-        await message.answer(str(diction))
-    except:
+        diction = json.dumps({"dataset": dataset, "labels": labels})
+        await message.answer(diction)
+    else:
         await message.answer('что то не так')
-
-
 
 
 async def main():
@@ -137,4 +112,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
